bfa342297e7aa13c3aab9f276dea2324c86f9b9a
songtutu <1172120378@qq.com>
2020-12-22 15:01:37 +0800

    feat: 初始化项目

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"gintest/routes"
+)
+
+func main() {
+	routes.InitRouter()
+}
---
+++ routes/router.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"gintest/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() {
+	gin.SetMode(utils.AppMode)
+	r := gin.Default()
+
+	router := r.Group("api/v1")
+	{
+		router.GET("hello", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "hello world",
+			})
+		})
+	}
+
+	r.Run(utils.HttpPort)
+}
